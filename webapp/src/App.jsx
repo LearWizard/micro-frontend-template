4ea8ca62e60dbbb95c88816a4db83ad4e109386d
Harsha99unifo <sriharsha.g@unifo.in>
2023-04-14 17:10:16 +0530

    removed unused files

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -6,9 +6,7 @@ import "./index.css";
 const App = () => (
   <div className="container">
     <div>Name: webapp</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Empty CSS</div>
+
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
